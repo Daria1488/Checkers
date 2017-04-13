9092c6acbb270545e377366e25898b50cc87cd35
Unknown <iviktor2614@gmail.com>
2017-04-13 17:50:02 +0300

    selected

--- views/site/index.php
+++ views/site/index.php
@@ -1,42 +1,6 @@
 <html>
 <head>
     <title>Шахматная доска</title>
-
-    <style>
-        .white{
-            background:white;
-            width:100px;
-            height:100px;
-            float:left;
-        }
-        .black{
-            background:#78a2b7;
-            width:100px;
-            height:100px;
-            float:left;
-        }
-        .border{
-            width: 810px;
-            height: 810px;
-            margin: auto;
-            border: solid 5px;
-        }
-        img{
-            width: 80px;
-            height: 80px;
-            margin-top: 10px;
-            margin-left: 10px;
-            cursor: pointer;
-            border-radius: 50px;
-            box-shadow: 8px 8px 7px rgba(0,0,0,0.5);
-            transition: all 0.5s ease;
-        }
-        img:hover{
-
-            box-shadow: 4px 4px 9px rgba(0,0,0,0.75);
-        }
-
-    </style>
 </head>
 <body>
 <div class="border">
--- web/js/game.js
+++ web/js/game.js
@@ -1,34 +1,49 @@
 var Game = {
-  start: function() {
+    $cells: [],
+    $container: null,
+    selected: null,
+    currentPlayer: null,
+    thisPlayer: {},
+    thatPlayer: {},
 
-  },
+    start: function() {
+        this.$container = $('.border');
 
-  end: function() {
+        this.$cells = [];
+        var $divs = $('.border div');
+        var num = 0;
+        for(var i = 0; i < 8; i++){
+            for(var j = 0; j < 8; j++){
+                $cells[i][j] = $divs.eq(num++).click(function() {
+                    if ($div.children('img').length) {
+                        Game.select(i, j);
+                    } else {
+                        Game.move(i, j);
+                    }
+                });
+            }
+        }
+    },
 
-  },
+    end: function() {
 
-  select: function($cell) {
-      this.selected = $cell;
-      this.$container.children().removeClass('selected');
-      $cell.addClass('selected');
-  },
+    },
 
-  move: function() {
+    select: function(x, y) {
+        if (this.selected) {
+            this.$cells[this.selected.x, this.selected.y].removeClass('selected');
+        }
+        this.selected = {x: x, y: y};
+        this.$cell[x, y].addClass('selected');
+    },
 
-  },
-
-  $container: $('.border'),
-  $selected: null
+    move: function() {
+        
+    },
 };
 
 $(function() {
-    $('.border div').each(function() {
-        var $div = $(this);
-
-        $div.click(function() {
-            if ($div.children('img').length) {
-                Game.select($div);
-            }
-        })
-    })
+    Game.start();
 });
+
+
