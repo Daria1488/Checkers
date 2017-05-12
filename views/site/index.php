cf013090313630f641f003b9dd7ac9998b66d588
Unknown <iviktor2614@gmail.com>
2017-05-12 17:47:08 +0300

    добавление скриптов(потом удалить комит)

--- views/site/index.php
+++ views/site/index.php
@@ -1,38 +1,40 @@
 <html>
 <head>
     <title>Шахматная доска</title>
+    <script type="text/javascript" src="jquery.js"></script>
 </head>
 <body>
+
 <div class="border">
 
-    <div class="black"><img src="/white.png"></div>
+    <div class="black pawn2"><img src="/white.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/white.png"></div>
+    <div class="black pawn2"><img src="/white.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/white.png"></div>
+    <div class="black pawn2"><img src="/white.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/white.png"></div>
+    <div class="black pawn2"><img src="/white.png"></div>
+    <div class="white"></div>
+
+    <div class="white"></div>    
+    <div class="black pawn2"><img src="/white.png"></div>
+    <div class="white"></div>
+    <div class="black pawn2"><img src="/white.png"></div>
+    <div class="white"></div>
+    <div class="black pawn2"><img src="/white.png"></div>
+    <div class="white"></div>
+    <div class="black pawn2"><img src="/white.png"></div>
+
+    <div class="black pawn2"><img src="/white.png"></div>
+    <div class="white"></div>
+    <div class="black pawn2"><img src="/white.png"></div>
+    <div class="white"></div>
+    <div class="black pawn2"><img src="/white.png"></div>
+    <div class="white"></div>
+    <div class="black pawn2"><img src="/white.png"></div>
     <div class="white"></div>
 
     <div class="white"></div>
-    <div class="black"><img src="/white.png"></div>
-    <div class="white"></div>
-    <div class="black"><img src="/white.png"></div>
-    <div class="white"></div>
-    <div class="black"><img src="/white.png"></div>
-    <div class="white"></div>
-    <div class="black"><img src="/white.png"></div>
-
-    <div class="black"><img src="/white.png"></div>
-    <div class="white"></div>
-    <div class="black"><img src="/white.png"></div>
-    <div class="white"></div>
-    <div class="black"><img src="/white.png"></div>
-    <div class="white"></div>
-    <div class="black"><img src="/white.png"></div>
-    <div class="white"></div>
-
-    <div class="white"></div>
     <div class="black"></div>
     <div class="white"></div>
     <div class="black"></div>
@@ -51,33 +53,50 @@
     <div class="white"></div>
 
     <div class="white"></div>
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
 
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
     <div class="white"></div>
 
     <div class="white"></div>
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
     <div class="white"></div>
-    <div class="black"><img src="/black.png"></div>
+    <div class="black pawn1"><img src="/black.png"></div>
 
 
 </div>
+<div class="indicator"><div>Игра не начата</div></div>
+
+<div class="message"><div>Ходить можно только вперёд по диагонали, бить можно как вперёд,
+        так и назад, если шашка дошла до другого конца, то она превращается в дамку. Бой обязателен.</div></div>
+
+<div class="timer hide"></div>
+
+<div class="shadow"><input type="button" class="start" value="Начать"/></div>
+
+<div class="score hide">Белые 12:12 Красные</div>
+
+<div class="win hide"><div></div></div>
+
+
+
+<script type="text/javascript" src="script.js"></script>
+</body>
 
 </html>
